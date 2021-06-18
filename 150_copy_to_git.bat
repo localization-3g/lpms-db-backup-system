d0e4a123377d9a5d68e4e00efa135ecfdc744858
MinoruK1234 <70566350+MinoruK1234@users.noreply.github.com>
2021-06-18 10:37:04 +0800

    update: variable name

--- 150_copy_to_git.bat
+++ 150_copy_to_git.bat
@@ -2,13 +2,13 @@ setlocal enabledelayedexpansion
 pushd %~dp0
 call setenv.bat
 
-set IMPORT_DIR=D:\Git\lpms-db\backup
 set BACKUP_DIR1=D:\LPMS_backup\backup
+set BACKUP_DIR2=D:\Git\lpms-db\backup
 pushd %BACKUP_DIR1%
 
 
-copy backup_date.txt  %IMPORT_DIR%\backup_date.txt
-copy *.json   %IMPORT_DIR%\
+copy backup_date.txt  %BACKUP_DIR2%\backup_date.txt
+copy *.json   %BACKUP_DIR2%\
 
 popd
 rem pause
