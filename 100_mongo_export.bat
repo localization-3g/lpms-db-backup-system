0962433f215f2cac39a22af3ed9ae81baa412fc4
fxy16124 <72133378+fxy16124@users.noreply.github.com>
2023-08-14 15:09:07 +0800

    Update 100_mongo_export.bat

--- 100_mongo_export.bat
+++ 100_mongo_export.bat
@@ -15,24 +15,26 @@ del -y emails.json
 del -y eml_files.json
 
 echo %YMD% %HMS% > backup_date.txt
-mongoexport.exe --uri="mongodb://10.98.84.13:27020/lpms"  --jsonArray --collection=documents --out=documents.json
-mongoexport.exe --uri="mongodb://10.98.84.13:27020/lpms"  --jsonArray --collection=forms     --out=forms.json
-mongoexport.exe --uri="mongodb://10.98.84.13:27020/lpms"  --jsonArray --collection=projects  --out=projects.json
-mongoexport.exe --uri="mongodb://10.98.84.13:27020/lpms"  --jsonArray --collection=users     --out=users.json
 mongoexport.exe --uri="mongodb://10.98.84.13:27020/lpms"  --jsonArray --collection=comments  --out=comments.json
+mongoexport.exe --uri="mongodb://10.98.84.13:27020/lpms"  --jsonArray --collection=documents --out=documents.json
 mongoexport.exe --uri="mongodb://10.98.84.13:27020/lpms"  --jsonArray --collection=emails    --out=emails.json
 mongoexport.exe --uri="mongodb://10.98.84.13:27020/lpms"  --jsonArray --collection=eml_files --out=eml_files.json
+mongoexport.exe --uri="mongodb://10.98.84.13:27020/lpms"  --jsonArray --collection=forms     --out=forms.json
+mongoexport.exe --uri="mongodb://10.98.84.13:27020/lpms"  --jsonArray --collection=reports   --out=reports.json
+mongoexport.exe --uri="mongodb://10.98.84.13:27020/lpms"  --jsonArray --collection=projects  --out=projects.json
+mongoexport.exe --uri="mongodb://10.98.84.13:27020/lpms"  --jsonArray --collection=users     --out=users.json
 rem pause
 
 call powershell Compress-Archive ^
      -Force -Path ^
-     .\documents.json,^
-     .\forms.json,^
-     .\projects.json,^
-     .\users.json,^
      .\comments.json ^
+     .\documents.json,^
      .\emails.json ^
      .\eml_files.json ^
+     .\forms.json,^
+     .\reports.json,^
+     .\projects.json,^
+     .\users.json,^
      -DestinationPath ^
      .\archived\LPMS_collection_%YMD%_%HMS%.zip
 
